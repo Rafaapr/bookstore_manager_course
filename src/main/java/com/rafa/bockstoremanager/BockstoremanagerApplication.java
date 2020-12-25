2c2501f9cdbc82eb947ca1db54a50e2005bd2b38
Rafa <rafa.log123@hotmail.com>
2020-12-25 19:52:35 -0300

    First commit projet structore created

---
+++ src/main/java/com/rafa/bockstoremanager/BockstoremanagerApplication.java
@@ -0,0 +1,13 @@
+package com.rafa.bockstoremanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BockstoremanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BockstoremanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rafa/bockstoremanager/BockstoremanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafa.bockstoremanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BockstoremanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
