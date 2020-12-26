105f5d3baac388beef723c51d050c959b3f16cae
Rafa <rafa.log123@hotmail.com>
2020-12-25 21:15:06 -0300

    Criado tabela e anotações para persistencia de bando de dados

---
+++ src/main/java/com/rafa/bockstoremanager/entity/Author.java
@@ -0,0 +1,15 @@
+package com.rafa.bockstoremanager.entity;
+
+import javax.persistence.*;
+
+@Entity
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, unique = true)
+    private String name;
+    @Column(nullable = false)
+    private Integer age;
+
+}
